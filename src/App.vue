67e2039d4f304b14ac1ef96207f205c378d60fc5
Ali60351 <ali60351@gmail.com>
2019-09-23 16:08:11 +0500

    refactor: eslint feat: add nav drawer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
-  <v-app>
-    <v-app-bar app>
+  <v-app :dark="dark">
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="toggleDrawer" />
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>THE</span>
+        <span class="font-weight-light">SOLUTIONISTS</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
-
+    <NavigationDrawer
+      ref="drawer"
+      @switchColor="toggleDark"
+    />
     <v-content>
       <router-view />
     </v-content>
@@ -22,10 +18,27 @@
 </template>
 
 <script>
+import NavigationDrawer from "./components/NavigationDrawer";
+
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    NavigationDrawer
+  },
+  beforeMount: function() {
+    this.$vuetify.theme.dark = true;
+  },
   data: () => ({
-    //
+    dark: false
   }),
+  methods: {
+    toggleDrawer: function() {
+      this.$refs.drawer.toggleDrawer();
+    },
+    toggleDark: function() {
+      this.dark = !this.dark;
+      this.$vuetify.theme.dark = this.dark;
+    }
+  }
 };
 </script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    persistent
+    fixed
+    app
+    mini-variant
+    clipped
+    enable-resize-watcher
+  >
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        ripple
+        :href="item.link"
+        @click="item.action"
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data: function() {
+    return {
+      drawer: true,
+      items: [
+        {
+          title: "New t",
+          icon: "mdi-github-circle",
+          action: () => {},
+          link: "#"
+        },
+        {
+          title: "New",
+          icon: "mdi-invert-colors",
+          action: this.switchColor,
+          link: "javascript:void(0)"
+        }
+      ]
+    };
+  },
+  methods: {
+    switchColor: function() {
+      this.$emit("switchColor");
+    },
+    toggleDrawer: function() {
+      this.drawer = !this.drawer;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
